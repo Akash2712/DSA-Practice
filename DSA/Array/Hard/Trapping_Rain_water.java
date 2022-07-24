c85ece059c7236ac047e3655ff6e3fa7cc680fec
Akash <palakash288@gmail.com>
2022-07-24 22:51:59 +0530

    commit

--- DSA/Array/Hard/Trapping_Rain_water.java
+++ DSA/Array/Hard/Trapping_Rain_water.java
@@ -3,7 +3,7 @@ package Hard;
 public class Trapping_Rain_water {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- DSA/Array/Medium/Search_in_2D_Matirx.java
+++ DSA/Array/Medium/Search_in_2D_Matirx.java
@@ -3,7 +3,7 @@ package Medium;
 public class Search_in_2D_Matirx {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
